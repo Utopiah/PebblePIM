609aa56fa3b945dad3815befc00f57425660d622
Fabien Benetou <fabien-services@benetou.fr>
2015-12-18 09:49:41 +0100

    basics forked from previous project

--- src/app.js
+++ src/app.js
@@ -1,60 +1,39 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
-var Vector2 = require('vector2');
+
+var ws = new WebSocket('ws://fabien.benetou.fr:8889');    // Replace with IP of computer running server
+
+var connected = false;
+
+ws.onopen = function () { 
+  ws.send('pebble sending memorable moment');
+  console.log('pebble sending memorable moment');
+  connected = true;
+};
+
 
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'PIM memorable moments',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  body: 'Press up for +1 and down for -1.'
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+ws.onmessage = function (event) { 
+ // should confirm that the message has been received
+  main.body(event.data);
+    console.log(event.data);
+};
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+main.on('click', function(e) {
+  main.subtitle('Button ' + e.button + ' pressed.');
+  if (connected){
+     ws.send(e.button + ' button pressed ');
+  }
+  // once the connection has been established the user has 1 minute to send
+  // +1 point using the up button
+  // -1 point using the up button
+  // the server at the end of the minute will write a new PmWiki file
+  // just like http://fabien.benetou.fr/Portfolio/CoEvolution?action=source
+  // those can then be listed at the end of the day or week to easilly spot the most intense moments
 });
