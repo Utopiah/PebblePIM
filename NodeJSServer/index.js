1ea9b3ff71c63d6b91793747f0f39eed940fa789
Fabien Benetou <fabien-services@benetou.fr>
2015-12-21 17:33:26 +0100

    Moved configuration to its own file

--- NodeJSServer/index.js
+++ NodeJSServer/index.js
@@ -1,7 +1,19 @@
 var fs = require('fs');
 
+var data = fs.readFileSync('./config.json'),
+    myObj, configuration;
+
+try {
+	configuration = JSON.parse(data);
+	console.dir(configuration);
+}
+catch (err) {
+	console.log('There has been an error parsing your JSON.')
+		console.log(err);
+}
+
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({host:"5.39.79.30", port:"8889"});
+  , wss = new WebSocketServer({host:configuration.wsip, port:configuration.port});
 
 wss.broadcast = function(data) {
   for (var i in this.clients)
@@ -9,11 +21,8 @@ wss.broadcast = function(data) {
 };
 console.log('PIM web socket server started');
 
-var pmWikiPath = "/home/web/benetou.fr/cloud/wiki/wiki.d/";
 var pmWikiVersion = "version=urlencoded=1";
 var pmWikiText = "";
-
-var pebbleAccountIDWebTest = '0123456789abcdef0123456789abcdef';
 
 var goodMomentIntensity = 0;
 var badMomentIntensity = 0;
@@ -22,6 +31,7 @@ var ids = new Array();
 // use like this:
 wss.on('connection', function(ws) {
   console.log('client connected');
+  wss.broadcast('Connected');
   ws.on('message', function(message) {
 		  console.log(message);
 
@@ -40,6 +50,8 @@ wss.on('connection', function(ws) {
   ws.on('close', function() {
 		  console.log('client disconnected');
 		  console.log('can now write the data to a Wiki file');
+		  if (ids[1] == configuration.pebbleAccountIDWeb) {
+		  console.log("proper ID, can write");
 		  console.log('at time '+ids[0]+' user '+ids[1]+' had an intense moment.');
 		  console.log('This moment was '+goodMomentIntensity+' good and '+badMomentIntensity+' bad.');
 		  // record to file
@@ -51,7 +63,7 @@ wss.on('connection', function(ws) {
 		  var ctime = ids[0];
 		  // the typical PmWiki calendar wouldn't work as several moments can appear per day
 
-		  var stream = fs.createWriteStream(pmWikiPath+filename);
+		  var stream = fs.createWriteStream(configuration.pmWikiPath+filename);
 		  stream.once('open', function(fd) {
 				  stream.write(pmWikiVersion+"\n");
 				  stream.write(pmWikiText+"\n");
@@ -60,6 +72,9 @@ wss.on('connection', function(ws) {
 				  });
 		  goodMomentIntensity = 0;
 		  badMomentIntensity = 0;
+		  } else {
+			  console.log("Improper ID, will NOT write.");
+		  }
   });
 
 });
